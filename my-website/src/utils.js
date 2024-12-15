87281d65f74c2950f3d898ae225f6f41a2c3b2c3
yolotouch <ungchan052804@gmail.com>
2024-12-15 20:54:47 +0900

    change

--- my-website/src/utils.js
+++ my-website/src/utils.js
@@ -1,5 +1,5 @@
 export const getImageUrl = (path) => {
     const base = new URL('./', import.meta.url); 
-    return new URL(`/assets/${path}`, base).href;
+    return new URL(`/Portfolio-Website/my-website/assets/${path}`, base).href;
   };
   
