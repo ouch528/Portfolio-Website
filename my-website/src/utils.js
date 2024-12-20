0fe8b689cfa5d633120a57e421f23233997509d7
yolotouch <ungchan052804@gmail.com>
2024-12-20 21:26:08 +0900

    change

--- my-website/src/utils.js
+++ my-website/src/utils.js
@@ -1,5 +1,8 @@
 export const getImageUrl = (path) => {
-    const base = new URL('./', import.meta.url); 
-    return new URL(`/Portfolio-Website/assets/${path}`, base).href;
-  };
-  
+  const base = import.meta.url;
+  const basePath = window.location.hostname === 'ouch528.github.io' 
+    ? '/my-website/' // Replace with your GitHub Pages repository name if different
+    : '/Portfolio-Website/'; // For local development, assuming root path
+
+  return new URL(`${basePath}assets/${path}`, base).href;
+};
